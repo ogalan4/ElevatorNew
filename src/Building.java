586ccaadc7a35b5b8a414f30cb423ab75455bf50
Алекс <ogalan4@gmail.com>
2022-03-16 20:32:16 +0300

    16.03.22

--- src/Building.java
+++ src/Building.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Buillding {
+public class Building {
     public static int countOfFloors;
     public static int numberOfWishedFloor;
-    public boolean isWelcomeByLogic = false;
-    public boolean isButtonPreset=false;
+    public boolean isButtonPreset = false;
 
     public void setCountOfFloors() {
         System.out.println("----------------------------------------------------------------------------");
-        System.out.println("|  Input the count of floors, which you want to have in the Buillding      |");
+        System.out.println("|  Input the count of floors, which you want to have in the Building      |");
         System.out.println("----------------------------------------------------------------------------");
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -25,7 +24,11 @@ public class Buillding {
         System.out.println("|  Input the number of floor, where you want to get elevator, please       |");
         System.out.println("|  If you want to quit the program, input 100                              |");
         System.out.println("----------------------------------------------------------------------------");
-        isButtonPreset=true;
+        isButtonPreset = true;
+
+        InputNumbersLogic inputNumbersLogic = new InputNumbersLogic();
+        inputNumbersLogic.waitFourSeconds();
+        numberOfWishedFloor= InputNumbersLogic.temp;
     }
 
     public void somebodyCallElevator() {
@@ -33,15 +36,5 @@ public class Buillding {
         System.out.println("|  If somebody else want to call elevator on some floor, input the appropriated number, please  |");
         System.out.println("|  If you want to quit the program, input 100                                                   |");
         System.out.println("-------------------------------------------------------------------------------------------------");
-    }
-
-    public void welcomeONFloorByDyrection() {
-        System.out.println("----------------------------------------------------------------------------");
-        System.out.println("|  Doors are open                                                          |");
-        System.out.println("|  Welcome on the " + numberOfWishedFloor + " floor                                                  |");
-        System.out.println("|  Doors are closed                                                        |");
-        System.out.println("----------------------------------------------------------------------------");
-        numberOfWishedFloor = 0;
-        isWelcomeByLogic = true;
     }
 }
